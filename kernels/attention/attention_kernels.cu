b8ffd45ee1a39e4ef9bb42bbdc1f8f7ed1e72170
awtrisk <113042016+awtrisk@users.noreply.github.com>
2025-01-20 11:19:41 +0000

    ?

--- kernels/attention/attention_kernels.cu
+++ kernels/attention/attention_kernels.cu
@@ -1219,12 +1219,12 @@ template <typename T, typename CACHE_T, int BLOCK_SIZE,
           int NUM_THREADS>
 void burst_attention_launcher(
     torch::Tensor& out, torch::Tensor& query, torch::Tensor& key_cache,
-    torch::Tensor& value_cache, int num_kv_heads, double scale,
-    torch::Tensor& block_tables, torch::Tensor& seq_lens, int max_seq_len,
+    torch::Tensor& value_cache, int64_t num_kv_heads, double scale,
+    torch::Tensor& block_tables, torch::Tensor& seq_lens, int64_t max_seq_len,
     const c10::optional<torch::Tensor>& alibi_slopes, double k_scale,
-    double v_scale, const int tp_rank, const int blocksparse_local_blocks,
-    const int blocksparse_vert_stride, const int blocksparse_block_size,
-    const int blocksparse_head_sliding_step,
+    double v_scale, const int64_t tp_rank, const int64_t blocksparse_local_blocks,
+    const int64_t blocksparse_vert_stride, const int64_t blocksparse_block_size,
+    const int64_t blocksparse_head_sliding_step,
     // IPC related parameters
     torch::Tensor& ipc_handles_tensor, // Tensor of IPC handles
     torch::Tensor& ipc_offsets_tensor, // Tensor of offsets
