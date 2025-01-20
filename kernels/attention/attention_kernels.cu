a39c61df7b3a07876e84dc15a776e88f650715ab
awtrisk <113042016+awtrisk@users.noreply.github.com>
2025-01-20 11:52:53 +0000

    from_float fix lt 2

--- kernels/attention/attention_kernels.cu
+++ kernels/attention/attention_kernels.cu
@@ -863,7 +863,7 @@ __global__ void burst_attention_kernel(
         for (int i = 0; i < NUM_ELEMS_PER_THREAD; ++i) {
             acc += accs[i];
         }
-        from_float(out[seq_idx * num_heads * HEAD_SIZE + head_idx * HEAD_SIZE], static_cast<float>(acc / exp_sum));
+        out[seq_idx * num_heads * HEAD_SIZE + head_idx * HEAD_SIZE] = from_float(static_cast<scalar_t>(acc / exp_sum));
     }
 
     // Cleanup: Close IPC handles
